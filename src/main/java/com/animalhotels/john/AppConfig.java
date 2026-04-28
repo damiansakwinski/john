43a83c1e9e35841debdcb8aa397429c48bd3d092
damiansakwinski <damiansakwinski@gmail.com>
2026-04-28 12:44:46 +0200

    fix(photo-classification): add timeouts to AppConfig

--- src/main/java/com/animalhotels/john/AppConfig.java
+++ src/main/java/com/animalhotels/john/AppConfig.java
@@ -2,10 +2,13 @@ package com.animalhotels.john;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
+
+import java.time.Duration;
 
 @Configuration
 public class AppConfig {
@@ -14,11 +17,21 @@ public class AppConfig {
         return BedrockRuntimeClient.builder()
             .credentialsProvider(DefaultCredentialsProvider.create())
             .region(Region.EU_CENTRAL_1)
+            .overrideConfiguration(c -> c
+                .apiCallTimeout(Duration.ofSeconds(60))
+                .apiCallAttemptTimeout(Duration.ofSeconds(30))
+            )
             .build();
     }
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder().build();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(5));
+        factory.setReadTimeout(Duration.ofSeconds(30));
+
+        return RestClient.builder()
+            .requestFactory(factory)
+            .build();
     }
 }
